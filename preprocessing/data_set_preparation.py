c2cc2a50134bed60f4209edabd72617f6baa3006
armin mohammadi <armin.mohamady@gmail.com>
2017-04-24 18:09:13 +0430

    Adds some initial data preparation scripts

--- preprocessing/data_set_preparation.py
+++ preprocessing/data_set_preparation.py
@@ -1,3 +1,28 @@
-"""DMC2017, 4/24/17, Sajad Azami"""
+import pandas as pd
+import numpy as np
+from pathlib import Path
+from preprocessing.data_preparation import read_data
 
-__author__ = 'sajjadaazami@gmail.com (Sajad Azami)'
+#merge items and train data to a dataFrame
+def merge_data(input_path, items_path, output_path):
+    tdf = read_data(input_path)
+    idf = read_data(items_path)
+    output = Path(output_path)
+    if not output.is_file():
+        mrg = pd.merge(tdf, idf)
+        pd.to_pickle(mrg, output_path)
+        return mrg
+    else:
+        return pd.read_pickle(output_path)
+
+# example of using merge_data function for train dataset
+mrg = merge_data('../data/train.csv', '../data/items.csv', '../data/train_merged.pkl')
+
+# uppercase all pharmForm values
+mrg['pharmForm'] = mrg['pharmForm'].str.upper()
+
+# add count feature (revenue/price)
+mrg['count'] = mrg.revenue/mrg.price
+
+# extract pharmForm values as binary feature and adding them to dataset
+mrg = pd.concat([mrg, pd.get_dummies(mrg['pharmForm'])], axis=1)
